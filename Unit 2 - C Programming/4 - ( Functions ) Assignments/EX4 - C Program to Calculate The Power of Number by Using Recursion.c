eb9a897ed80a064185fd8491f4624e0c4bda1b66
Youssef Adel <youssefadel1502@gmail.com>
2022-06-13 03:13:24 +0200

    Add Functions Lec Assignments

---
+++ Unit 2 - C Programming/4 - ( Functions ) Assignments/EX4 - C Program to Calculate The Power of Number by Using Recursion.c
@@ -0,0 +1,26 @@
+/**==================================================
+--    Author     :   YOUSSEF ADEL YOUSSEF
+--  Description  :   Calculate The Power of Number by Using Recursion
+==================================================**/
+
+#include <stdio.h>
+
+int Power(int n , int p)
+{
+	if(p != 1)
+		return n * Power(n , p-1);
+}
+
+int main()
+{
+	int num , pow , val ;
+	printf("Enter Base number : ");
+	fflush(stdin);    fflush(stdout);
+	scanf("%d" , &num );
+	printf("Enter Power number (Positive Integer) : ");
+	fflush(stdin);    fflush(stdout);
+	scanf("%d" , &pow );
+	val = Power(num , pow);
+	printf("%d^%d = %d" , num , pow , val);
+	return 0;
+}
