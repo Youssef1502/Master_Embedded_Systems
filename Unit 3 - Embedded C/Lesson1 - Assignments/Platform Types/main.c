6ef14551c4a57f78b2e39c2245c1315e27961848
Youssef Adel <youssefadel1502@gmail.com>
2022-06-13 03:16:50 +0200

    Add Lesson 1 Assignments

---
+++ Unit 3 - Embedded C/Lesson1 - Assignments/Platform Types/main.c
@@ -0,0 +1,13 @@
+/**==================================================
+--    Author     :   YOUSSEF ADEL YOUSSEF
+--  Description  :   Write a C Program to Build a Platform Types
+==================================================**/
+
+#include<stdio.h>
+#include"Platform_Types.h"
+
+int main()
+{
+	return 0;
+}
+
