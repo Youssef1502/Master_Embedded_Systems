c82809477305b4e7d520af419bc50cbf8579f496
Youssef Adel <youssef.adel.elawady@gmail.com>
2022-06-26 22:16:51 +0200

    Add Unit 7 Lesson 1 Labs

---
+++ Unit 7 - (MCU Essential Peripherals )/Lesson 1 - Labs/Lab 3/Memory_Map.h
@@ -0,0 +1,16 @@
+/**==================================================
+--    Author     :   YOUSSEF ADEL YOUSSEF
+--  Description  :   Write a C Program to Count form 0 to 9 on 7-Segment
+                     by Using ATMEGA32 Microcontroller
+==================================================**/
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+#define DDRC    *((volatile unsigned char *)(0x34))
+#define PORTC    *((volatile unsigned char *)(0x35))
+
+
+
+#endif /* INCFILE1_H_ */
---
+++ Unit 7 - (MCU Essential Peripherals )/Lesson 1 - Labs/Lab 3/main.c
@@ -0,0 +1,26 @@
+/**==================================================
+--    Author     :   YOUSSEF ADEL YOUSSEF
+--  Description  :   Write a C Program to Count form 0 to 9 on 7-Segment
+                     by Using ATMEGA32 Microcontroller
+==================================================**/
+
+#include "Memory_Map.h"
+#include <util/delay.h>
+
+#define  F_CPU 8000000UL  // Clock Configuration
+
+int main(void)
+{
+	DDRC = 0xff;
+	unsigned int i , j;
+	
+    while (1) 
+    {
+		for(i=0 ; i<10 ; i++)
+		{
+			PORTC = (0b1011) | (i<<4);
+			_delay_ms(3000);
+		}
+    }
+}
+
